a77f3aacaf1ae5e3d65bce2d865be931664e9005
Ulukele <36236949+Ulukele@users.noreply.github.com>
2021-03-20 20:50:15 +0700

    add solution for lab8-0

--- lab8-0/src/main.c
+++ lab8-0/src/main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
 #include <malloc.h>
 
 typedef struct {
@@ -6,6 +8,98 @@ typedef struct {
     int End;
     int Weight;
 } TEdge;
+
+typedef struct {
+    TEdge* Edges;
+    int VerticesCount;
+    int EdgesCount;
+} TGraph;
+
+typedef struct {
+    int* Parent;
+    int* Rank;
+} TDSU;
+
+void DeleteSets(TDSU* dsu) {
+    if (dsu == NULL) {
+        return;
+    }
+    free(dsu->Parent);
+    free(dsu->Rank);
+    free(dsu);
+}
+
+TDSU* MakeSets(int count) {
+    TDSU* dsu = malloc(sizeof(TDSU));
+    if (dsu == NULL) {
+        return NULL;
+    }
+    dsu->Parent = calloc(count, sizeof(int));
+    dsu->Rank = calloc(count, sizeof(int));
+   if (dsu->Parent == NULL || dsu->Rank == NULL) {
+        DeleteSets(dsu);
+        return NULL;
+    }
+    for (int i = 0; i < count; ++i) {
+        dsu->Parent[i] = i;
+    }
+    return dsu;
+}
+
+int FindSet(TDSU* dsu, int element) {
+    if (element == dsu->Parent[element]) {
+        return element;
+    }
+    dsu->Parent[element] = FindSet(dsu, dsu->Parent[element]);
+    return dsu->Parent[element];
+}
+
+void MergeSets(TDSU* dsu, int firstSet, int secondSet) {
+    firstSet = FindSet(dsu, firstSet);
+    secondSet = FindSet(dsu, secondSet);
+    if (dsu->Rank[firstSet] > dsu->Rank[secondSet]) {
+        dsu->Parent[secondSet] = firstSet;
+        return;
+    }
+    dsu->Parent[firstSet] = secondSet;
+    if (dsu->Rank[firstSet] == dsu->Rank[secondSet]) {
+        dsu->Rank[secondSet] += 1;
+    }
+}
+
+int CompareEdges(const TEdge* first, const TEdge* second) {
+    return first->Weight - second->Weight;
+}
+
+void SortEdges(TEdge* edges, int edgesCount) {
+    qsort(edges, edgesCount, sizeof(TEdge), (int(*) (const void*, const void*))CompareEdges);
+}
+
+TEdge* FindSpanningTree(TGraph* graph) {
+    TEdge* edges = graph->Edges;
+    int edgesCount = graph->EdgesCount;
+    int verticesCount = graph->VerticesCount;
+
+    TEdge* spanningTree = calloc(verticesCount - 1, sizeof(*spanningTree));
+    int spanningTreeLen = 0;
+    if (spanningTree == NULL) {
+        return spanningTree;
+    }
+
+    SortEdges(edges, edgesCount);
+    TDSU* dsu = MakeSets(verticesCount);
+    for (int i = 0; i < edgesCount; ++i) {
+        int from = edges[i].Begin;
+        int to = edges[i].End;
+        if (FindSet(dsu, from) != FindSet(dsu, to)) {
+            MergeSets(dsu, from, to);
+            spanningTree[spanningTreeLen] = edges[i];
+            spanningTreeLen++;
+        }
+    }
+    DeleteSets(dsu);
+    return spanningTree;
+}
 
 int main() {
     int n, m;
@@ -26,7 +120,9 @@ int main() {
         return 0;
     }
 
-    TEdge* edges = malloc(sizeof(*edges) * m);
+    int edgesCount = 0;
+    TEdge* edges = calloc(m, sizeof(*edges));
+    assert(edges);
     for (int i = 0; i < m; ++i) {
         int from, to, weight;
         if (scanf("%d %d %d", &from, &to, &weight) != 3) {
@@ -39,8 +135,29 @@ int main() {
             free(edges);
             return 0;
         }
+        edges[edgesCount].Begin = from - 1;
+        edges[edgesCount].End = to - 1;
+        edges[edgesCount].Weight = weight;
+        edgesCount++;
     }
 
+    TGraph graph;
+    graph.Edges = edges;
+    graph.VerticesCount = n;
+    graph.EdgesCount = m;
+    
+    TEdge* spanningTree = FindSpanningTree(&graph);
+    if (spanningTree == NULL) {
+        free(edges);
+        return 0;
+    }
 
+    for (int i = 0; i < n - 1; ++i) {
+        printf("%d %d\n", spanningTree[i].Begin + 1, spanningTree[i].End + 1);
+    }
+
+    free(edges);
+    free(spanningTree);
+    
     return 0;
 }
